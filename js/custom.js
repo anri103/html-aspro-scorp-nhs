a0e7352853d87ad4569b060c59d6ca49dad3740d
anri103 <ianrianri@gmail.com>
2024-08-06 02:10:26 +0300

    -

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+
+// Функция для анимации полосы прогресса
+// function animateProgressBar(slider) {
+//     var $progressBar = $(slider).find('.progress-hero-bar');
+//     $progressBar.stop(true, true).css('width', '0');
+//     $progressBar.animate({
+//         width: '100%'
+//     }, slider.vars.slideshowSpeed, 'linear');
+// }
+
+// Функция для сброса полосы прогресса
+// function resetProgressBar(slider) {
+//     var $progressBar = $(slider).find('.progress-hero-bar');
+//     $progressBar.stop(true, true).css('width', '0');
+// }
